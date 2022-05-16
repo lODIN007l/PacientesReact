67e60b7018574e14a5b876d7c5ef3762ea6791d3
lODIN007l <wilsonlluilema007@gmail.com>
2022-05-15 21:57:27 -0500

    advertensia

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -6,7 +6,27 @@ import {useState,useEffect} from 'react'
 function Formulario() {
 
   const [nombre,setNombre]=useState('')
-  
+  const [propietario,setPropietario]=useState('')
+  const [email,setEmail]=useState('')
+  const [fecha,setFecha]=useState('')
+  const [sintomas,setSintomas]=useState('')
+  const [error,setError]=useState(false)
+
+
+  const handleSubmit = (e)=> {
+    e.preventDefault()
+
+    //validnado formulario 
+    if ([nombre,propietario,email,fecha,sintomas].includes('')) {
+      console.log('hay al meenos un campo vacio ')
+      setError(true)
+      return
+    }
+    setError(false)
+
+
+    console.log("enviando formulario")
+  }
 
 
 
@@ -25,7 +45,18 @@ function Formulario() {
            Administralos
          </span>
        </p>
-       <form  className='bg-white shadow-md rounded-lg py-10 px-5 mb-10 ml-5 mr-5'>
+       <form   
+          onSubmit={handleSubmit}
+          className='bg-white shadow-md rounded-lg py-10 px-5 mb-10 ml-5 mr-5'
+        >
+          {error&&(
+            <div className='bg-red-700 p-3 text-white text-center uppercase font-bold rounded mb-3'>
+              <p>
+                Todos los campos son obligatorios 
+              </p>
+            </div>
+
+          )}
          <div className='mb-5'>
            <label  htmlFor='mascota' className='block text-gray-500 uppercase font-bold'>Nombre Mascota</label>
            <input 
@@ -45,6 +76,8 @@ function Formulario() {
               type="text" 
               placeholder='Nombre del propietario' 
               className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md '
+              value={propietario}
+              onChange={(e)=>setPropietario(e.target.value)}
               />
          </div>
          <div className='mb-5'>
@@ -54,6 +87,8 @@ function Formulario() {
               type="email" 
               placeholder='Correo de contacto' 
               className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md '
+              value={email}
+              onChange={(e)=>setEmail(e.target.value)}
               />
          </div>
          <div className='mb-5'>
@@ -63,6 +98,8 @@ function Formulario() {
               type="date" 
               
               className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md '
+              value={fecha}
+              onChange={(e)=>setFecha(e.target.value)}
               />
          </div>
          <div className='mb-5'>
@@ -72,13 +109,17 @@ function Formulario() {
            <textarea   
            className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md ' 
               id='sintomas'
-               placeholder='Describe los sintomas' />
+               placeholder='Describe los sintomas' 
+               value={sintomas}
+               onChange={(e)=>setSintomas(e.target.value)}
+               />
          </div>
 
-        <input type="submit "
-        value={'agregar paciente'}
+        <button 
         className='bg-indigo-600 w-full p-3 text-white text-center uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-all'
-        />
+        >
+          AGREGAR PACIENTE
+        </button>
        </form>
       </div>
   )
